9ebcd8a071a14a7918a257fabfef1f3660282827
Alan <alan.edward.martin@gmail.com>
2022-06-17 17:27:11 -0400

    build: add husky, add linters, add prettier, vscode settings

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next Starter</title>
+      </Head>
+      Nice and empty...
+    </div>
+  );
+}
